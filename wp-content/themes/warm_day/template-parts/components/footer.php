28366e490d664bfe8099b20d5f2fec427c165ecf
Taurta <taurta@vk.com>
2022-12-04 23:02:06 +0300

    Добавлены контакты отвественных

--- wp-content/themes/warm_day/template-parts/components/footer.php
+++ wp-content/themes/warm_day/template-parts/components/footer.php
@@ -5,32 +5,37 @@
         <h2>Контакты и обратная связь</h2>
         <div class="contacts-container">
             <form id="feedback" class="request js-request">
-                <input type="hidden" name="action" value="feedback">
                 <h3>Форма обратной связи</h3>
-                <label for="">
-                    Имя
-                    <input type="text" name="name" id="feedback_name" required>
-                </label>
-                <label for="">
-                    Телефон
-                    <input type="tel" name="phone" id="feedback_phone" required pattern="\+7 \d{3} \d{3} \d{2} \d{2}|8 \d{3} \d{3} \d{2} \d{2}">
-                </label>
-                <label for="">
-                    Электронная почта
-                    <input type="email" name="email" id="feedback_email" required>
-                </label>
-                <label for="">
-                    Вопрос
-                    <textarea name="text" id="feedback_text" cols="30" rows="5"></textarea>
-                </label>
-                <button type="submit">
-                    Отправить
-                    <svg width="24" height="12" viewBox="0 0 24 12" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M0 6H23M23 6L18.0714 1M23 6L18.0714 11" stroke="white"/>
-                    </svg>
-                </button>
-                <input type="checkbox" id="feedback_pp" checked required>
-                <label for="feedback_pp">Даю согласие на&nbsp<a href="/privacy-policy" target="_blank">обработку персональных данных</a></label>
+                <div class="feedback-col">
+                    <input type="hidden" name="action" value="feedback">
+                    <label for="">
+                        Имя
+                        <input type="text" name="name" id="feedback_name" required>
+                    </label>
+                    <label for="">
+                        Телефон
+                        <input type="tel" name="phone" id="feedback_phone" required pattern="\+7 \d{3} \d{3} \d{2} \d{2}|8 \d{3} \d{3} \d{2} \d{2}">
+                    </label>
+                    <label for="">
+                        Электронная почта
+                        <input type="email" name="email" id="feedback_email" required>
+                    </label>
+                </div>
+                <div class="feedback-col">
+                    <label for="">
+                        Вопрос
+                        <textarea name="text" id="feedback_text" cols="30" rows="5"></textarea>
+                    </label>
+                    <button type="submit">
+                        Отправить
+                        <svg width="24" height="12" viewBox="0 0 24 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M0 6H23M23 6L18.0714 1M23 6L18.0714 11" stroke="white"/>
+                        </svg>
+                    </button>
+                    <input type="checkbox" id="feedback_pp" checked required>
+                    <label for="feedback_pp">Даю согласие на&nbsp<a href="/privacy-policy" target="_blank">обработку персональных данных</a></label>
+                </div>
+
                 <div class="form_preloader">
                     <div class="lds-dual-ring"></div>
                 </div>
@@ -39,8 +44,8 @@
                 </div>
             </form>
             <div class="acceptance-points-conteiner">
+                <h3>Пункты приема подарков</h3>
                 <div class="acceptance-points">
-                    <h3>Пункты приема подарков</h3>
                     <?php if ($allCities) : ?>
                         <?php foreach ($allCities as $city) : ?>
                             <?php if (!empty($city['point']['address'])) : ?>
@@ -51,11 +56,17 @@
                                     <div class="point-title">
                                         <?= $city['name'] ?>
                                     </div>
-                                    <?php foreach ($city['point']['address'] as $address) : ?>
-                                        <div class="point-text">
-                                            <span><?= $address['city'] ?></span> – <?= $address['address'] ?>
-                                        </div> 
-                                    <?php endforeach; ?>                                
+                                    <div class="point-text">
+                                        <?php foreach ($city['point']['address'] as $address) : ?>
+                                            <span><?= $address['city'] ?></span> – <?= $address['address'] ?><br>
+                                        <?php endforeach; ?>   
+                                        <?php if (!empty($city['point']['fio'])) : ?>
+                                            <br><span>Ответственный(ая): </span><?= $city['point']['fio'] ?>
+                                            <?php if (!empty($city['point']['phone_number'])) : ?>
+                                                <br><span>тел: </span><?= $city['point']['phone_number'] ?>
+                                            <?php endif; ?>
+                                        <?php endif; ?>
+                                    </div>                             
                                 </div>
                             <?php endif; ?>
                         <?php endforeach; ?>
